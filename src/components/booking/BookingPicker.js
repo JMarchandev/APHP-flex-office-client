9f1e6e5b1bef3e86cbb52ca175bfebe4b7ee0318
JM <j.marchand.dev@gmail.com>
2020-10-14 09:00:41 +0200

    morning commit

--- src/components/booking/BookingPicker.js
+++ src/components/booking/BookingPicker.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
 //Internal imports
-import { getToken } from "../../services/utils/getToken";
-import { getCurrentUser } from "../../services/api/getCurrentUser";
-import { getAvailableRoomsByDate } from "../../services/api/events/getAvailableRoomsByDate"
-import { getFormartedDate } from "../../services/utils/getFormartedDate"
+import {getToken} from "../../services/utils/getToken";
+import {getCurrentUser} from "../../services/api/getCurrentUser";
+import {getAvailableRoomsByDate} from "../../services/api/events/getAvailableRoomsByDate"
+import {getFormartedDate} from "../../services/utils/getFormartedDate"
 
 //External imports
 import axios from 'axios'
@@ -14,140 +14,142 @@ import 'react-calendar/dist/Calendar.css';
 
 
 const PlanningPicker = () => {
-  const [error, setError] = useState(null)
-  const [success, setSuccess] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
-  const [selectDateFormat, setSelectDateFormat] = useState(null)
-  const [currentDate, setCurrentDate] = useState(new Date())
-  const [selectDateRooms, setSelectDateRooms] = useState(null)
-  const [availableRooms, setAvailableRooms] = useState([])
-  const [selectedRoom, setSelectedRoom] = useState(null)
-  const [selectDate, setSelectDate] = useState(null)
-  const [currentUser, setCurrentUser] = useState([])
+   const [error, setError] = useState(null)
+   const [success, setSuccess] = useState(null)
+   const [isLoading, setIsLoading] = useState(true)
+   const [selectDateFormat, setSelectDateFormat] = useState(null)
+   const [currentDate, setCurrentDate] = useState(new Date())
+   const [selectDateRooms, setSelectDateRooms] = useState(null)
+   const [availableRooms, setAvailableRooms] = useState([])
+   const [selectedRoom, setSelectedRoom] = useState(null)
+   const [selectDate, setSelectDate] = useState(null)
+   const [currentUser, setCurrentUser] = useState([])
 
-  useEffect(async () => {
-    getCurrentUser().then(response => {
-      setCurrentUser(response.data)
-    }).catch(error => {
-      console.log(error)
-    })
-  }, [])
-
-  const handleClickChooseDate = (value) => {
-    const dateFormated = getFormartedDate(value)
-
-    setSelectDateFormat(dateFormated)
-    setSelectDate(value)
-
-    if (value < currentDate) {
-      setError('Cette date est déjà passée')
-      setSelectDate(null)
-      setAvailableRooms(null)
-      return null;
-    } else if (value >= currentDate) {
-      setError(null)
-    }
-
-    value.setMinutes(value.getMinutes() - value.getTimezoneOffset());
-    const dateParam = value.toISOString().split('T')[0];
-
-    getAvailableRoomsByDate(dateParam, currentUser.id)
-      .then(response => {
-        setError(null)
-        setAvailableRooms(response.data)
+   useEffect(async () => {
+      getCurrentUser().then(response => {
+         setCurrentUser(response.data)
+      }).catch(error => {
+         console.log(error)
       })
-      .catch(error => {
-        setAvailableRooms(null)
-        setError("Une reservation a déja été faite ce jour")
-      })
+   }, [])
 
-  }
+   const handleClickChooseDate = (value) => {
+      const dateFormated = getFormartedDate(value)
 
-  const handleChooseRoom = (event, room) => {
-    event.preventDefault()
-    setSelectedRoom(room)
-  }
+      setSelectDateFormat(dateFormated)
+      setSelectDate(value)
 
-  const handleSubmitBooking = e => {
-    e.preventDefault()
-
-    selectDate.setMinutes(selectDate.getMinutes() - selectDate.getTimezoneOffset());
-    const dateParam = selectDate.toISOString().split('T')[0];
-
-    const requestBody = {
-      eventDate: dateParam,
-      user_id: currentUser.id,
-      room: selectedRoom.id
-    }
-
-    const config = {
-      headers: {
-        Authorization: `Bearer ${getToken()}`
+      if (value < currentDate) {
+         setError('Cette date est déjà passée')
+         setSelectDate(null)
+         setAvailableRooms(null)
+         return null;
+      } else if (value >= currentDate) {
+         setError(null)
       }
-    }
 
-    axios.post('http://localhost:1337/events', qs.stringify(requestBody), config)
-      .then(response => setSuccess('Votre reservation a été ajouté'))
-      .catch(error => console.log(error))
-  }
+      value.setMinutes(value.getMinutes() - value.getTimezoneOffset());
+      const dateParam = value.toISOString().split('T')[0];
 
-  return (
-    <div>
-      {error ?
-        <div className="mt-1 alert alert-danger" role="alert">
-          {error}
-        </div>
-        : ""
+      getAvailableRoomsByDate(dateParam, currentUser.id)
+         .then(response => {
+            setError(null)
+            setAvailableRooms(response.data)
+         })
+         .catch(error => {
+            setAvailableRooms(null)
+            setError("Une reservation a déja été faite ce jour")
+         })
+
+   }
+
+   const handleChooseRoom = (event, room) => {
+      event.preventDefault()
+      setSelectedRoom(room)
+   }
+
+   const handleSubmitBooking = e => {
+      e.preventDefault()
+
+      selectDate.setMinutes(selectDate.getMinutes() - selectDate.getTimezoneOffset());
+      const dateParam = selectDate.toISOString().split('T')[0];
+
+      const requestBody = {
+         eventDate: dateParam,
+         user_id: currentUser.id,
+         room: selectedRoom.id
       }
-      {success ?
-        <div className="mt-1 alert alert-success d-flex justify-content-between align-items-center" role="alert">
-          {success}
-          <a href="/" className="btn btn-success">Mes reservations</a>
-        </div>
-        : ""
+
+      const config = {
+         headers: {
+            Authorization: `Bearer ${getToken()}`
+         }
       }
-      <h3 className="my-2">Planning <small className="text-muted">(personnel)</small></h3>
-      <h5>Choisissez une date</h5>
-      <div className="d-flex border-dark mt-3">
-        <div className="w-75 mr-2">
-          <Calendar
-            className="w-100"
-            onClickDay={handleClickChooseDate}
-            value={currentDate}
-          />
-        </div>
-        <div className="w-50 pl-2">
-          <h5 className="text-center">Choisissez une salle</h5>
-          {availableRooms ?
-            <div>
-              <ul className="d-flex flex-wrap list-unstyled w-100">
-                {availableRooms.map(room => (
-                  <li className="w-50 p-1" key={room.id}>
-                    <button onClick={event => handleChooseRoom(event, room)} className="btn btn-primary w-100">{room.roomIdentifier}</button>
-                  </li>
-                ))}
-              </ul>
-              {selectDateFormat ?
-                <div className="card m-1">
-                  <div className="card-body">
-                    <h5 className="card-title">Récapitulatif</h5>
-                    <p className="card-text">Date: {selectDateFormat}</p>
-                    <p className="card-text">Salle: {selectedRoom ? selectedRoom.roomIdentifier : "Selectionnez une salle"}</p>
-                    <div className="d-flex justify-content-between">
-                      {selectedRoom && selectDate ?
-                        <button onClick={event => handleSubmitBooking(event)} className="btn btn-primary">Reserver</button>
-                        : ""}
-                      {success ? <a href="/" className="btn btn-success">Mes reservations</a> : ""}
-                    </div>
-                  </div>
-                </div>
-                : ""}
+
+      axios.post('http://localhost:1337/events', qs.stringify(requestBody), config)
+         .then(response => setSuccess('Votre reservation a été ajouté'))
+         .catch(error => console.log(error))
+   }
+
+   return (
+      <div>
+         {error ?
+            <div className="mt-1 alert alert-danger" role="alert">
+               {error}
             </div>
-            : ""}
-        </div>
+            : ""
+         }
+         {success ?
+            <div className="mt-1 alert alert-success d-flex justify-content-between align-items-center" role="alert">
+               {success}
+               <a href="/" className="btn btn-success">Mes reservations</a>
+            </div>
+            : ""
+         }
+         <h3 className="my-2">Planning <small className="text-muted">(personnel)</small></h3>
+         <h5>Choisissez une date</h5>
+         <div className="d-flex border-dark mt-3">
+            <div className="w-75 mr-2">
+               <Calendar
+                  className="w-100"
+                  onClickDay={handleClickChooseDate}
+                  value={currentDate}
+               />
+            </div>
+            <div className="w-50 pl-2">
+               <h5 className="text-center">Choisissez une salle</h5>
+               {availableRooms ?
+                  <div>
+                     <ul className="d-flex flex-wrap list-unstyled w-100">
+                        {availableRooms.map(room => (
+                           <li className="w-50 p-1" key={room.id}>
+                              <button onClick={event => handleChooseRoom(event, room)}
+                                      className="btn btn-primary w-100">{room.roomIdentifier}</button>
+                           </li>
+                        ))}
+                     </ul>
+                     {selectDateFormat ?
+                        <div className="card m-1">
+                           <div className="card-body">
+                              <h5 className="card-title">Récapitulatif</h5>
+                              <p className="card-text">Date: {selectDateFormat}</p>
+                              <p className="card-text">Salle: {selectedRoom ? selectedRoom.roomIdentifier : "Selectionnez une salle"}</p>
+                              <div className="d-flex justify-content-between">
+                                 {selectedRoom && selectDate ?
+                                    <button onClick={event => handleSubmitBooking(event)}
+                                            className="btn btn-primary">Reserver</button>
+                                    : ""}
+                                 {success ? <a href="/" className="btn btn-success">Mes reservations</a> : ""}
+                              </div>
+                           </div>
+                        </div>
+                        : ""}
+                  </div>
+                  : ""}
+            </div>
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
 export default PlanningPicker;
--- src/components/myPlanning/Planning.js
+++ src/components/myPlanning/Planning.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 //External imports
 import BigCalendar from 'react-big-calendar';
@@ -7,133 +7,188 @@ import moment from 'moment';
 import 'moment/min/locales.min'
 
 //Internal imports
-import { getCurrentUser } from '../../services/api/getCurrentUser';
-import { getMyEvents } from '../../services/api/events/getMyEvents';
-import { getFormartedDate } from '../../services/utils/getFormartedDate';
+import {getCurrentUser} from '../../services/api/getCurrentUser';
+import {getMyEvents} from '../../services/api/events/getMyEvents';
+import {getFormartedDate} from '../../services/utils/getFormartedDate';
+import Calendar from "react-calendar";
 
 moment.locale('fr');
 const localizer = BigCalendar.momentLocalizer(moment);
 const allViews = Object
-    .keys(BigCalendar.Views)
-    .map(k => BigCalendar.Views[k])
+   .keys(BigCalendar.Views)
+   .map(k => BigCalendar.Views[k])
 
 const Planning = () => {
-    const [myEvents, setMyEvents] = useState(null)
-    const [currentUser, setCurrentUser] = useState([])
-    const [eventObject, setEventObject] = useState(null)
-    const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString('fr-FR', { year: 'numeric', month: 'long' }))
-    const [detailCard, setDetailCard] = useState(null)
+   const [error, setError] = useState(null)
+   const [isEditing, setIsEditing] = useState(true)
+   const [myEvents, setMyEvents] = useState(null)
+   const [currentUser, setCurrentUser] = useState([])
+   const [eventObject, setEventObject] = useState(null)
+   const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString('fr-FR', {
+      year: 'numeric',
+      month: 'long'
+   }))
+   const [detailCard, setDetailCard] = useState(null)
 
-    useEffect(() => {
-        async function getEvents() {
-            getCurrentUser()
-                .then(response => {
-                    console.log(response);
-                    setCurrentUser(response.data)
+   useEffect(() => {
+      async function getEvents() {
+         getCurrentUser()
+            .then(response => {
+               console.log(response);
+               setCurrentUser(response.data)
 
-                    getMyEvents(response.data.id)
-                        .then(response => {
-                            console.log(response.data);
-                            setMyEvents(response.data)
-                            let object = []
+               getMyEvents(response.data.id)
+                  .then(response => {
+                     console.log(response.data);
+                     setMyEvents(response.data)
+                     let object = []
 
-                            for (let i = 0; i < response.data.length; i++) {
-                                let constructObject = {
-                                    title: response.data[i].room.roomIdentifier,
-                                    start: response.data[i].eventDate,
-                                    end: response.data[i].eventDate,
-                                    allDay: true,
-                                    ressource: {
-                                        id: response.data[i].room.id,
-                                        address: response.data[i].room.address,
-                                        batiment: response.data[i].room.batiment,
-                                        city: response.data[i].room.city,
-                                        zipCode: response.data[i].room.zipcode,
-                                        floor: response.data[i].room.floor,
-                                        capacity: response.data[i].room.sitPlace
-                                    }
-                                }
-                                object.push(constructObject)
-                            }
-                            setEventObject(object);
-                        })
-                        .catch(error => {
-                            console.log(error)
-                        })
-                })
-                .catch(error => {
-                    console.log(error)
-                });
-        }
-        getEvents()
-    }, [])
-    const handleEventClick = (event) => {
-        console.log(event);
-        setDetailCard({
-            id: event.ressource.id,
-            title: event.title,
-            start: event.start,
-            end: event.end,
-            allDay: event.allDay,
-            address: event.ressource.address,
-            batiment: event.ressource.batiment,
-            city: event.ressource.city,
-            zipCode: event.ressource.zipCode,
-            floor: event.ressource.floor,
-            capacity: event.ressource.capacity
-        })
+                     for (let i = 0; i < response.data.length; i++) {
+                        let constructObject = {
+                           title: response.data[i].room.roomIdentifier,
+                           start: response.data[i].eventDate,
+                           end: response.data[i].eventDate,
+                           allDay: true,
+                           ressource: {
+                              id: response.data[i].room.id,
+                              address: response.data[i].room.address,
+                              batiment: response.data[i].room.batiment,
+                              city: response.data[i].room.city,
+                              zipCode: response.data[i].room.zipcode,
+                              floor: response.data[i].room.floor,
+                              capacity: response.data[i].room.sitPlace
+                           }
+                        }
+                        object.push(constructObject)
+                     }
+                     setEventObject(object);
+                  })
+                  .catch(error => {
+                     console.log(error)
+                  })
+            })
+            .catch(error => {
+               console.log(error)
+            });
+      }
+
+      getEvents()
+   }, [])
+
+   const handleEventClick = (event) => {
+      console.log(event);
+      setDetailCard({
+         id: event.ressource.id,
+         title: event.title,
+         start: event.start,
+         end: event.end,
+         allDay: event.allDay,
+         address: event.ressource.address,
+         batiment: event.ressource.batiment,
+         city: event.ressource.city,
+         zipCode: event.ressource.zipCode,
+         floor: event.ressource.floor,
+         capacity: event.ressource.capacity
+      })
+   }
+
+   const handleEditingClick = event => {
+      event.preventDefault()
+      setIsEditing(true)
 
 
-    }
+   }
 
-    console.log(detailCard)
-    const messages = {
-        allDay: 'journée',
-        previous: 'précédent',
-        next: 'suivant',
-        today: 'Mois en cours',
-        month: 'mois',
-        week: 'semaine',
-        day: 'jour',
-        agenda: 'Agenda',
-        date: 'date',
-        time: 'heure',
-        event: 'événement',
-        Sun: 'Dimanche', // Or anything you want
-        showMore: total => `+ ${total} événement(s) supplémentaire(s)`
-    }
+   const messages = {
+      allDay: 'journée',
+      previous: 'précédent',
+      next: 'suivant',
+      today: 'Mois en cours',
+      month: 'mois',
+      week: 'semaine',
+      day: 'jour',
+      agenda: 'Agenda',
+      date: 'date',
+      time: 'heure',
+      event: 'événement',
+      Sun: 'Dimanche', // Or anything you want
+      showMore: total => `+ ${total} événement(s) supplémentaire(s)`
+   }
 
-    return (
-        <div className="container">
-            <div>
-                {detailCard ?
-                    <div class="card my-3">
-                        <h5 class="card-header">Votre réservation du {detailCard.start}</h5>
-                        <div class="card-body">
-                            <h5 class="card-title">Details</h5>
-                            <p class="card-text">Salle: {detailCard.title} / Adresse: {detailCard.address} / Ville: {detailCard.city} / Code Postal: {detailCard.zipCode}</p>
-                            <button class="btn btn-primary">Go somewhere</button>
-                        </div>
-                    </div>
-                    : ""}
+   return (
+      <div className="container">
+         <div>
+            <div className="d-flex">
+               <div className="w-75 mr-2">
+                  <Calendar
+                     className="w-100"
+                     onClickDay={console.log('coucou')}
+                     value={new Date()}
+                  />
+               </div>
+               <div className="w-25 ml-2">
+                  <form>
+                     <div className="form-group">
+                        <label htmlFor="exampleFormControlSelect1">Example select</label>
+                        <select className="form-control" id="exampleFormControlSelect1">
+                           <option>1</option>
+                           <option>2</option>
+                           <option>3</option>
+                           <option>4</option>
+                           <option>5</option>
+                        </select>
+                     </div>
+                     <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"
+                               placeholder="Password"/>
+                     </div>
+                     <div className="form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me
+                           out</label>
+                     </div>
+                     <button type="submit" className="btn btn-primary">Submit</button>
+                  </form>
+               </div>
             </div>
-            <div style={{ height: '80vh' }}>
-                <h4 className="text-center my-3">{currentDate}</h4>
-                {eventObject ?
-                    <BigCalendar
-                        messages={messages}
-                        localizer={localizer}
-                        events={eventObject}
-                        step={60}
-                        views={'month'}
-                        defaultDate={new Date()}
-                        toolbar={false}
-                        onSelectEvent={(event) => handleEventClick(event)}
-                    />
-                    : ""}
-            </div>
-        </div>
-    )
+            {detailCard ?
+               <div className="card my-3">
+                  <h5 className="card-header">Votre réservation du {detailCard.start}</h5>
+                  <div className="card-body">
+                     <h5 className="card-title">Details</h5>
+                     <p className="card-text">Salle: {detailCard.title} / Adresse: {detailCard.address} /
+                        Ville: {detailCard.city} / Code Postal: {detailCard.zipCode}</p>
+                     <div>
+                        {isEditing ?
+                          <p>in progress</p>
+                           : ""}
+                        <button className="btn btn-danger float-right m-2">Supprimer</button>
+                        <button onClick={event => handleEditingClick(event)}
+                                className="btn btn-warning float-right m-2">Editer
+                        </button>
+                     </div>
+                  </div>
+               </div>
+               : ""}
+         </div>
+         <div style={{height: '80vh'}}>
+            <h4 className="text-center my-3">{currentDate}</h4>
+            {eventObject ?
+               <BigCalendar
+                  messages={messages}
+                  localizer={localizer}
+                  events={eventObject}
+                  step={60}
+                  views={'month'}
+                  defaultDate={new Date()}
+                  toolbar={false}
+                  onSelectEvent={(event) => handleEventClick(event)}
+               />
+               : ""}
+         </div>
+      </div>
+   )
 }
 
 export default Planning;
